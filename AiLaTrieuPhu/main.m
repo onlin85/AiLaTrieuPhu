8779491339adad0556762c473c594c017a7b1d17
Hong Pham <onlin85@yahoo.com>
2013-08-01 15:30:24 +0700

    Create new project
    Create an empty project

---
+++ AiLaTrieuPhu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AiLaTrieuPhu
+//
+//  Created by Hồng Phạm on 01/08/2013.
+//  Copyright (c) 2013 IOS_02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
